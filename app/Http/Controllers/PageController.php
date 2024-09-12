42507399814e36fb5734a3a8216389aea6e0cf60
ludoff <ludovicofiore25@gmail.com>
2024-09-12 16:58:44 +0200

    feat: add model and print results in page

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Train;
 
 
 class PageController extends Controller
@@ -20,6 +21,14 @@ class PageController extends Controller
 
     public function contacts(){
         return view('contacts');
+    }
+
+
+    public function trains(){
+
+        $trains= Train::all();
+
+        return view('trains', compact('trains'));
     }
 }
 
---
+++ resources/views/trains.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts.main')
+
+
+
+@section('content')
+    <div class="container my-5">
+        <h2>Treni</h2>
+
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Azienda</th>
+                    <th scope="col">Stazione di partenza</th>
+                    <th scope="col">Stazione di arrivo</th>
+                    <th scope="col">Orario di partenza</th>
+                    <th scope="col">Orario di arrivo</th>
+                    <th scope="col">Codice Treno</th>
+                    <th scope="col">Numero Carrozze</th>
+                    <th scope="col">Ritardo</th>
+                    <th scope="col">Cancellato</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($trains as $train)
+                    <tr>
+                        <th>{{ $train->id }}</th>
+                        <td>{{ $train->company }}</td>
+                        <td>{{ $train->departure_station }}</td>
+                        <td>{{ $train->arrival_station }}</td>
+                        <td>{{ $train->departure_time }}</td>
+                        <td>{{ $train->arrival_time }}</td>
+                        <td>{{ $train->train_code }}</td>
+                        <td>{{ $train->train_carriage }}</td>
+                        <td>{{ $train->on_time }}</td>
+                        <td>{{ $train->deleted }}</td>
+                    </tr>
+                @endforeach
+
+            </tbody>
+        </table>
+    </div>
+@endsection
+
+
+@section('titlePage')
+    treni
+@endsection
