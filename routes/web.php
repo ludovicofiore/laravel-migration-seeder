1e21fa46be63bbcd2158c46a756306a2980dc485
ludovico fiore <166535968+ludovicofiore@users.noreply.github.com>
2024-09-12 15:24:29 +0200

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/',[PageController::class,'index'])->name('home');
+Route::get('/chi-siamo',[PageController::class,'about'])->name('about');
+Route::get('/contatti',[PageController::class,'contacts'])->name('contacts');
